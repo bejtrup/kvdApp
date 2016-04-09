6ee837a88b6db376e0a74b09204e80c45877e713
daniel <bejtrup@gmail.com>
2016-04-09 09:04:49 +0200

    styl

--- www/js/index.js
+++ www/js/index.js
@@ -140,8 +140,7 @@ var app = {
 
         $("#share").click(function(){
             var pos_link = wordOne+" "+wordTwo;
-            window.plugins.socialsharing.share('NAVN siger jer er i : '+ pos_link +'                                         Åben Kvdapp for at finde mig, eller brug dette link (linket kræver net) ', null, null, 'http://bejtrup.github.io/tag/#'+pos_link+'/');
-            
+            window.plugins.socialsharing.share('RUNE siger jeg er i : '+ pos_link +'                                         Åben RUNE appen for at finde mig eller brug dette link (linket kræver lidt net) ', null, null, 'http://bejtrup.github.io/tag/#'+pos_link+'/');
         });
 
         function makeWord(x) {
