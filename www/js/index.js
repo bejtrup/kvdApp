186de478f82e4bc7ec46430aa55e6a4cd753200e
daniel <bejtrup@gmail.com>
2016-06-22 18:54:08 +0200

    android test

--- www/js/index.js
+++ www/js/index.js
@@ -65,6 +65,8 @@ var app = {
         var words = ['And','Angreb','Anke','Apparat','Arm','Bag','Balance','Balle','Kartoffel','Ballon','Banan','Bande','Kul','Lille','Basker','Kano','Beton','Bil','Konto','Krus','Mad','Bokse','Pen','Blind','Gynge','Bror','Brun','Brød','Bast','Buks','Hus','Bunke','Misse','Potte','Bøjle','Musik','Bøtte','Mor','Chef','Chili','Chips','Corn','Creme','Kajak','Damp','Os','Kontra','Dild','Direktør','Dog','Slæde','Dressing','Drift','isme','Durum','Dyne','Dåse','Filt','Genert','Falde','Juice','Fedte','Feeling','Filet','Finger','Fis','Fisk','Fiske','Fjeder','Flad','Flamme','Flæske','Fløjte','Svær','Fold','Fræk','Stød','Frisk','Fjeder','Fylde','Fars','Fører','Gane','Gas','General','Svine','Glasur','Glat','Greb','Tisse','Gris','Grotte','Gruppe','Sved','Gummi','Gylle','Brand','Gæst','Sprække','Tyr','Hals','Hammer','Los','Handske','Hav','Smat','Heste','Hipster','Hjelm','Hot','Hud','Hul','Hule','Trolde','Buller','Svans','Hævner','Hønse','Hår','Svamp','Indianer','Tunge','Tyk','Jern','Junge','Kage','Dame','Kamel','Kammer','Kanal','Basse','Karl','Fælles','Kasse','Kaster','Knold','Stram','Klister','Kloak','Klods','Bjarke','Dej','Krig','Blod','Krydder','Krølle','Kugle','Barberet','Kuling','Kurv','Kutter','Kvæler','Kys','Kæde','Kæmpe','Kæp','Kød','Sex','Kølle','Kost','Pavillon','Tog','Skam','Leg','Øje','Lejr','Lem','Uld','Bas','Liste','Lokum','Luft','Lugte','Lus','Læbe','Løb','Løs','Lag','Blodig','Skede','Dele','Mande','Maskine','Massage','Medister','Bæ','Charlatan','Thai','Mund','Bøsse','Mælke','Metal','Nakke','Nisse','Næse','Nøgen','Okse','Ophæng','Orange','Daniel','Ost','Glide','Parfume','Patter','Bolle','Penge','Pige','Spyd','Piske','Pølse','Plante','Pleje','Plæne','Mis','Postej','Bæver','Præmie','Pukkel','Pul','Pumpe','Pung','Piller','Ribs','Ring','Rom','Rotte','Rulle','Sår','Ryste','Ræs','Ræve','Rød','Salat','Salt','Salve','Sans','Køle','Side','Sigøjner','Sjover','Lap','Makker','Skib','Skjuler','Sko','Skole','Skorpe','Skud','Skygge','Skæg','Skål','Slag','Slam','Slange','Slap','Slik','Herre','Smæld','Smør','So','Soft','Sol','Sort','Sovs','Spalte','Spejder','Gård','Pind','Stamme','Stang','Stativ','Kiosk','Stiv','Store','Stål','Sukker','Inder','Hvid','Græs','Giver','Fod','Søster','Søvn','Runde','Tag','Tagselv','Tand','Tank','Tarm','Tartelet','Telt','Tennis','Motor','Greve','Lår','Toilet','Plastik','Top','Torske','Traktor','Hunde','Tromme','Trommel','Trop','Træ','Træk','Trænings','Tun','Is','Tur','Jagt','Halm','Tyv','Tårn','Ulve','Under','Unge','Ur','Varm','Vin','Vorte','Væske','Yngel','Yngle','Ældre','Leif','Økonomi','Øl','Gul','Lygte'];
 
 //,'Glas','Flaske','Gigolo','Sofa','Pude','Kat','Serviet','Skærm','Stof','Lighter','Filt','Denim','Guld','Sølv','Velour','Dreng','Garn','Mørke','Vand','Dun' DOLK
+var AEOEAAwords = ['Slæde','Flæske','Svær','Fræk','Gæst','Sprække','Hævner','Fælles','Kvæler','Kæde','Kæmpe','Kæp','Læbe','Bæ','Mælke','Næse','Ophæng','Plæne','Bæver','Præmie','Ræs','Ræve','Skæg','Smæld','Græs','Træ','Træk','Trænings','Væske','Brød','Bøjle','Bøtte','Direktør','Fløjte','Stød','Fører','Hønse','Krølle','Kød','Kølle','Løb','Løs','Bøsse','Nøgen','Pølse','Rød','Køle','Sigøjner','Smør','Søster','Søvn','Dåse','Hår','Sår','Skål','Gård','Stål','Lår','Tårn','Ældre','Øje','Økonomi','Øl'];
+var AEOEAAwordsTranslate = ['Slaede','Flaeske','Svaer','Fraek','Gaest','Spraekke','Haevner','Faelles','Kvaeler','Kaede','Kaempe','Kaep','Laebe','Bae','Maelke','Naese','Ophaeng','Plaene','Baever','Praemie','Raes','Raeve','Skaeg','Smaeld','Graes','Trae','Traek','Traenings','Vaeske','Broed','Boejle','Boette','Direktoer','Floejte','Stoed','Foerer','Hoense','Kroelle','Koed','Koelle','Loeb','Loes','Boesse','Noegen','Poelse','Roed','Koele','Sigoejner','Smoer','Soester','Soevn','Daase','Haar','Saar','Skaal','Gaard','Staal','Laar','Taarn','aeldre','oeje','oekonomi','oel'];
 
           var suggestionsWordone = []
           $.each(words, function(k, v){
@@ -155,7 +157,6 @@ var app = {
 
         }
 
-
         $("#fire").click(function(){
             fire();
             sug.html('');
@@ -167,8 +168,10 @@ var app = {
         });
 
         $("#share").click(function(){
-            var pos_link = wordOne+"-"+wordTwo;
-            window.plugins.socialsharing.share('Rune påstår jeg er i : '+ wordOne+","+wordTwo +'. Åbn Rune-appen for at finde mig eller brug dette link (linket kræver lidt data) ', null, null, 'http://www.runeapp.dk/#'+pos_link+'/');
+            var shareOne = checkWordAndTranslate($.trim(wordOne));
+            var shareTwo = checkWordAndTranslate($.trim(wordTwo));
+
+            window.plugins.socialsharing.share('Rune påstår jeg er i : '+ wordOne+","+wordTwo +'. Åbn Rune-appen for at finde mig eller brug dette link (linket kræver lidt data) ', null, null, 'http://www.runeapp.dk/#'+shareOne+"-"+shareTwo+'/');
             sug.html('');
         });
 
